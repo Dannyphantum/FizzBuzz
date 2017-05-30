0e185f2ea7b3751bb74291a3ada94fa892e5c530
Your Name <youremail@domain.com>
2017-05-30 11:23:29 -0400

    first commit

---
+++ src/FizzBuzz.java
@@ -0,0 +1,23 @@
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Outputs string Fizz Buzz
+		for(int i = 1; i < 101; i++){
+			
+			if(i%3 == 0){
+				System.out.println("Fizz");
+			} 
+			else if(i%5 == 0){
+				System.out.println("Buzz");
+			}
+			else if(i%15 == 0){
+				System.out.println("FizzBuzz");
+			}
+			else System.out.println(i);
+		}
+				
+	}
+
+}
